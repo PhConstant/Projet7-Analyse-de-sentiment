f904f2826de0305d4854d2880ff9c9bb6193d5ec
PhConstant <constant.ensta@gmail.com>
2025-10-11 21:51:22 +0200

    Update API files with tflite BERT model

--- API/app_bert.py
+++ API/app_bert.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import pandas as pd
+import numpy as np
 from Source.preprocess_data import preprocess_data_embedding
 from transformers import AutoTokenizer
 import tensorflow as tf
-import joblib
 import logging
 import os
 
@@ -26,14 +26,28 @@ class FeedbackIn(BaseModel):
     user_feedback: str   # "Oui" ou "Non"
     right_answer: bool
 
-# Charger le modèle "Production" depuis le MLflow Model Registry (ou chemin local)
-MODEL_PATH = os.path.join("exp_models","Best_BERT_model2") 
-print(f"Loading model from {MODEL_PATH}")
-TOKENIZER_PATH = os.path.join("exp_models","Best_BERT_tokenizer")
-print(f"Loading tokenizer from {TOKENIZER_PATH}")
-model = tf.saved_model.load(MODEL_PATH)
-infer = model.signatures["serving_default"]
+
+# Chargement du tokenizer HF
+TOKENIZER_PATH = os.path.join("exp_models","final_tokenizer")
 tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
+print(f"Loading tokenizer from {TOKENIZER_PATH}")
+MAX_LEN = 64
+THRESHOLD = 0.45034 # Seuil optimal après conversion tflite float16
+# Charger le modèle TFLite float16
+MODEL_PATH = os.path.join("exp_models","bert_model_f16.tflite")
+print(f"Loading model from {MODEL_PATH}")
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+print("Model loaded.")
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+print(f"Sequence length for model: {MAX_LEN} tokens")
+interpreter.resize_tensor_input(input_details[0]['index'], [1, MAX_LEN])
+interpreter.resize_tensor_input(input_details[1]['index'], [1, MAX_LEN])
+interpreter.allocate_tensors()
+print("Interpreter allocated.")
+
+
 
 
 logging.basicConfig(level=logging.INFO)
@@ -45,23 +59,19 @@ def predict(inp: TweetIn):
     print("Text input raw :")
     print(inp.text)
 
-    encoded_input = tokenizer(
-        inp.text,
-        padding=True,
-        truncation=True,
-        return_tensors='tf'
-    )
+    inputs = tokenizer(inp.text, return_tensors="np", padding='max_length', max_length=MAX_LEN, truncation=True)
 
-    output = infer(**{
-    "input_ids": encoded_input["input_ids"],
-    "attention_mask": encoded_input["attention_mask"],
-    "token_type_ids": encoded_input.get("token_type_ids", tf.zeros_like(encoded_input["input_ids"]))
-    })
+    interpreter.set_tensor(input_details[0]['index'], inputs['attention_mask'])
+    interpreter.set_tensor(input_details[1]['index'], inputs['input_ids'])
 
-    proba = output['output_0'].numpy()[0][0]
+    # Inference
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details[0]['index'])
+
+    proba = np.array(output[0])[0]
     print(f"Probabilité calculée : {round(proba, 5)}")
 
-    return PredOut(positive_proba=proba, positive=proba>=0.5)
+    return PredOut(positive_proba=proba, positive=proba>=THRESHOLD)
 
 
 @app.post("/feedback")
